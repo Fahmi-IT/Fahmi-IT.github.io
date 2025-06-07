c9875f946dc43c59c0793acc84b113251d257e4a
Fahmi-IT <sajidfahmi03@gmail.com>
2025-06-07 18:52:16 -0400

    added onclick redirect to project 1

--- portfolio.js
+++ portfolio.js
@@ -2,3 +2,7 @@ function openPage(pageName) {
     const url = `${pageName}.html`;
     window.open(url, '_blank');
 }
+
+function openProject(projectName) {
+    window.open(projectName, '_blank');
+}
